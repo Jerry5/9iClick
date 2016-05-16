6b16f3c655fb34a80fb5a03bfa5c3878ea5228da
JiaFang <jerry@wuzhiyuandeimac.local>
2016-05-16 10:34:52 +0800

    Initial Commit

---
+++ 9iClick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  9iClick
+//
+//  Created by 武志远 on 16/5/16.
+//  Copyright © 2016年 武志远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 9iClick/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  9iClick
+//
+//  Created by 武志远 on 16/5/16.
+//  Copyright © 2016年 武志远. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
